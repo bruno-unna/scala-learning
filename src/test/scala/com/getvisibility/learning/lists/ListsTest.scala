df1149615c46aa93dc0a386ab5c7017a84a97556
Bruno Unna <bunna@getvisibility.com>
2020-01-22 11:08:38 +0000

    Write unit tests for `pack`

--- src/test/scala/com/getvisibility/learning/lists/ListsTest.scala
+++ src/test/scala/com/getvisibility/learning/lists/ListsTest.scala
@@ -46,4 +46,21 @@ class ListsTest extends AnyWordSpec {
       }
     }
   }
+  "A list of symbols" when {
+    "full" should {
+      "return a packed list" in {
+        assertResult(
+          List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+        )(
+          Lists.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+        )
+      }
+    }
+    "empty" should {
+      "return an empty list" in {
+        assert(Lists.pack(List()).isEmpty)
+      }
+    }
+  }
+
 }
